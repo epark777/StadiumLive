1ce420d1d2a4f741fa0f9fc24251bc5b08f4dbc4
Elliot <parkelliot777@gmail.com>
2024-10-14 20:04:50 -0700

    nav and profile button

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,33 +1,55 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { FaUserCircle } from 'react-icons/fa';
 import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
-     const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
 
-     const logout = (e) => {
-          e.preventDefault();
-          dispatch(sessionActions.logout());
-     };
+  const toggleMenu = (e) => {
+    e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
+    // if (!showMenu) setShowMenu(true);
+    setShowMenu(!showMenu);
+  };
 
-     return (
-          <>
-               <button>
-                    <FaUserCircle />
-               </button>
-               <ul className="profile-dropdown">
-                    <li>{user.username}</li>
-                    <li>
-                         {user.firstName} {user.lastName}
-                    </li>
-                    <li>{user.email}</li>
-                    <li>
-                         <button onClick={logout}>Log Out</button>
-                    </li>
-               </ul>
-          </>
-     );
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (ulRef.current && !ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener('click', closeMenu);
+  }, [showMenu]);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+  return (
+    <>
+      <button onClick={toggleMenu}>
+        <FaUserCircle />
+      </button>
+      <ul className={ulClassName} ref={ulRef}>
+        <li>{user.username}</li>
+        <li>{user.firstName} {user.lastName}</li>
+        <li>{user.email}</li>
+        <li>
+          <button onClick={logout}>Log Out</button>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default ProfileButton;
