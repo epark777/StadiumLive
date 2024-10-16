2d467a9e42c728afc1d3aab97859a9eba3dac51a
Elliot <parkelliot777@gmail.com>
2024-10-15 22:19:30 -0700

    navbar

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -2,22 +2,41 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import { MdOutlineStadium } from 'react-icons/md';
+
+// <MdOutlineStadium />
 
 function Navigation({ isLoaded }) {
-  const sessionUser = useSelector(state => state.session.user);
+     const sessionUser = useSelector((state) => state.session.user);
 
-  return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      {isLoaded && (
-        <li>
-          <ProfileButton user={sessionUser} />
-        </li>
-      )}
-    </ul>
-  );
+     return (
+          <div className="navbar">
+               <NavLink to="/">
+                    <div className="logo">
+                         <MdOutlineStadium />
+                         <a>StadiumLive</a>
+                    </div>
+               </NavLink>
+               {isLoaded && (
+                    <div className="profile-button">
+                         <ProfileButton user={sessionUser} />
+                    </div>
+               )}
+          </div>
+     );
 }
 
 export default Navigation;
+
+{
+     /* <ul>
+<li>
+  <NavLink to="/">Home</NavLink>
+</li>
+{isLoaded && (
+  <li>
+    <ProfileButton user={sessionUser} />
+  </li>
+)}
+</ul> */
+}
