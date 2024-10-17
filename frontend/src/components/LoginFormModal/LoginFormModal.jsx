aa01580893d897713d931057c574617f0c0c1bb3
Elliot <parkelliot777@gmail.com>
2024-10-16 17:55:06 -0700

    demo button

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -5,54 +5,71 @@ import { useModal } from '../../context/Modal';
 import './LoginForm.css';
 
 function LoginFormModal() {
-  const dispatch = useDispatch();
-  const [credential, setCredential] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState({});
-  const { closeModal } = useModal();
+     const dispatch = useDispatch();
+     const [credential, setCredential] = useState('');
+     const [password, setPassword] = useState('');
+     const [errors, setErrors] = useState({});
+     const { closeModal } = useModal();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setErrors({});
-    return dispatch(sessionActions.login({ credential, password }))
-      .then(closeModal)
-      .catch(async (res) => {
-        const data = await res.json();
-        if (data && data.errors) {
-          setErrors(data.errors);
-        }
-      });
-  };
+     const handleSubmit = (e) => {
+          e.preventDefault();
+          setErrors({});
+          return dispatch(sessionActions.login({ credential, password }))
+               .then(closeModal)
+               .catch(async (res) => {
+                    const data = await res.json();
+                    if (data && data.errors) {
+                         setErrors(data.errors);
+                    }
+               });
+     };
 
-  return (
-    <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username or Email
-          <input
-            type="text"
-            value={credential}
-            onChange={(e) => setCredential(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.credential && (
-          <p>{errors.credential}</p>
-        )}
-        <button type="submit">Log In</button>
-      </form>
-    </>
-  );
+     const disable = (credential, password) => {
+          if (credential.length < 4 || password.length < 6) {
+               return true;
+          } else {
+               return false;
+          }
+     };
+
+     return (
+          <div className="login-page">
+               <h1>Log In</h1>
+               <form onSubmit={handleSubmit}>
+                    <label>
+                         <input
+                              type="text"
+                              placeholder='Username or Email'
+                              value={credential}
+                              onChange={(e) => setCredential(e.target.value)}
+                              required
+                         />
+                    </label>
+                    <label>
+                         <input
+                              type="password"
+                              placeholder='password'
+                              value={password}
+                              onChange={(e) => setPassword(e.target.value)}
+                              required
+                         />
+                    </label>
+                    {errors.credential && <p>{errors.credential}</p>}
+                    <button
+                         type="submit"
+                         disabled={disable(credential, password)}
+                    >
+                         Log In
+                    </button>
+                    <button 
+                    type='submit'
+                    onClick={() => {
+                      setCredential('demo@user.io')
+                      setPassword('password')
+                    }}>Login to Demo</button>
+               </form>
+          </div>
+     );
 }
 
 export default LoginFormModal;
