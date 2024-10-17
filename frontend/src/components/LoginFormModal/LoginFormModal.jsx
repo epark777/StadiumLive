557783663bd1773a66b6c44146fb71b34a8925c1
Elliot <parkelliot777@gmail.com>
2024-10-16 18:48:06 -0700

    login page

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -5,71 +5,71 @@ import { useModal } from '../../context/Modal';
 import './LoginForm.css';
 
 function LoginFormModal() {
-     const dispatch = useDispatch();
-     const [credential, setCredential] = useState('');
-     const [password, setPassword] = useState('');
-     const [errors, setErrors] = useState({});
-     const { closeModal } = useModal();
+   const dispatch = useDispatch();
+   const [credential, setCredential] = useState('');
+   const [password, setPassword] = useState('');
+   const [errors, setErrors] = useState({});
+   const { closeModal } = useModal();
 
-     const handleSubmit = (e) => {
-          e.preventDefault();
-          setErrors({});
-          return dispatch(sessionActions.login({ credential, password }))
-               .then(closeModal)
-               .catch(async (res) => {
-                    const data = await res.json();
-                    if (data && data.errors) {
-                         setErrors(data.errors);
-                    }
-               });
-     };
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      setErrors({});
+      return dispatch(sessionActions.login({ credential, password }))
+         .then(closeModal)
+         .catch(async (res) => {
+            const data = await res.json();
+            if (data && data.errors) {
+               setErrors(data.errors);
+            }
+         });
+   };
 
-     const disable = (credential, password) => {
-          if (credential.length < 4 || password.length < 6) {
-               return true;
-          } else {
-               return false;
-          }
-     };
+   const disable = (credential, password) => {
+      if (credential.length < 4 || password.length < 6) {
+         return true;
+      } else {
+         return false;
+      }
+   };
 
-     return (
-          <div className="login-page">
-               <h1>Log In</h1>
-               <form onSubmit={handleSubmit}>
-                    <label>
-                         <input
-                              type="text"
-                              placeholder='Username or Email'
-                              value={credential}
-                              onChange={(e) => setCredential(e.target.value)}
-                              required
-                         />
-                    </label>
-                    <label>
-                         <input
-                              type="password"
-                              placeholder='password'
-                              value={password}
-                              onChange={(e) => setPassword(e.target.value)}
-                              required
-                         />
-                    </label>
-                    {errors.credential && <p>{errors.credential}</p>}
-                    <button
-                         type="submit"
-                         disabled={disable(credential, password)}
-                    >
-                         Log In
-                    </button>
-                    <button 
-                    type='submit'
-                    onClick={() => {
-                      setCredential('demo@user.io')
-                      setPassword('password')
-                    }}>Login to Demo</button>
-               </form>
-          </div>
-     );
+   return (
+      <div className="login-page">
+         <h1 className='login-header'>Log In</h1>
+         <form onSubmit={handleSubmit} className='login-form'>
+            <label className='login-label'>
+               <input
+                  type="text"
+                  placeholder="Username or Email"
+                  value={credential}
+                  onChange={(e) => setCredential(e.target.value)}
+                  required
+               />
+            </label>
+            <label className='login-label'>
+               <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+               />
+            </label>
+            {errors.credential && <p>{errors.credential}</p>}
+            <button type="submit" disabled={disable(credential, password)} className='login-button'>
+               Log In
+            </button>
+            <button
+               type="button"
+               onClick={() => {
+                  setCredential('demo@user.io');
+                  setPassword('password');
+               }} className='login-demo-button'
+            >
+               Login to Demo
+            </button>
+         </form>
+      </div>
+   );
 }
 
 export default LoginFormModal;
--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -7,29 +7,29 @@ import { MdOutlineStadium } from 'react-icons/md';
 // <MdOutlineStadium />
 
 function Navigation({ isLoaded }) {
-     const sessionUser = useSelector((state) => state.session.user);
+   const sessionUser = useSelector((state) => state.session.user);
 
-     return (
-          <div className="navbar">
-               <NavLink to="/">
-                    <div className="logo">
-                         <MdOutlineStadium />
-                         <a>StadiumLive</a>
-                    </div>
-               </NavLink>
-               {isLoaded && (
-                    <div className="profile-button">
-                         <ProfileButton user={sessionUser} />
-                    </div>
-               )}
-          </div>
-     );
+   return (
+      <div className="navbar">
+         <NavLink to="/">
+            <div className="logo">
+               <MdOutlineStadium />
+               <a>StadiumLive</a>
+            </div>
+         </NavLink>
+         {isLoaded && (
+            <div className="profile-button">
+               <ProfileButton user={sessionUser} />
+            </div>
+         )}
+      </div>
+   );
 }
 
 export default Navigation;
 
 {
-     /* <ul>
+   /* <ul>
 <li>
   <NavLink to="/">Home</NavLink>
 </li>
